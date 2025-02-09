abae1e7edefb4f3a5a079e1ed02f51245e112075
Katal Vlad <kavladpost@gmail.com>
2025-02-09 20:13:41 +0800

    1.2.2
    Добавил двухмерный массив.

--- Пробный/js/script.js
+++ Пробный/js/script.js
@@ -1,12 +1,16 @@
 //Создаём поле размером 330х330
 //Клетка будет 33х33
-
-let field_x = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И'];
-let field_y = [0,1,2,3,4,5,6,7,8,9];
-let field_xy = [];
-var a = 0;
-var b = 0;
-
+let field_xy = [
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9]
+];
 //document.getElementById('title')
 /*
 const body = document.body;
@@ -50,23 +54,12 @@ var wrapperBlock = document.getElementById("wrapperBlock");
 window.onload = function(){ //Функция которая создаёт поле 10х2 (10х10)
   for(var i=0; i<10; i++){
     var square = document.createElement("div");
-    
-    if(a<10){
-        c= field_x[a]+field_y[b]
-        field_xy.push(
-            [field_x[a], field_y[b]] 
-        );
-        a= a+1
-    }; if(a==10){
-        a= a-10
-        b= b+1
-    }
     //square.id = c //Даём каждому div свой ID
     /*if(a<10){
         console.log("Координаты в массиве", c)
     };*/
     square.classList.add("square");
-    square.innerText =c;
+    square.innerText =" ";
     wrapperBlock.appendChild(square);
   }
 }
