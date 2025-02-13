76ec4a9e261acac227dfbaaece2ad8ce64127c88
Katal Vlad <kavladpost@gmail.com>
2025-02-13 23:24:58 +0800

    Неудача.
    Хотел, сделать по лучше код, но у меня не получилось.

--- Пробный/js/script.js
+++ Пробный/js/script.js
@@ -8,23 +8,53 @@ let field_xy = [
     [0,0,0,0,0,"Tr1",0,0,0,0],
     [0,0,0,0,0,"Tr1",0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    [1,0,1,1,0,0,"Ch","Ch","Ch","Ch"],
+    [0,0,0,0,0,0,"Ch","Ch","Ch","Ch"],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0]
 ];
 
-
+let field_CanShot= [
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,]
+]
 //Корабли и их ХП (кол-во целых палуб.)
+
+//Массив, в котором хронятся данные: (0)Имя, (1)кол-во палуб или оставшихся палуб, а в дальнейшем Id div-ов, чтобы осуществить пометку убитого коробля. 
+/*data_ship = [
+    ["on1",1],["on2",1],["on3",1],["on4",1], //Однопалубники
+    ["db1",2],["db2",2],["db3",2], //Двухпалубники
+    ["Tr1",3],["Tr2",3],//Трёхпалубники
+    ["Ch",4]//Однопалубники
+]*/
+//Массив для регистрации попадания по кораблю.
+let hit_ship = [
+    [],[],[],[], //0-3   - Однопалубники
+    [],[],[],    //4-6   - Двухпалубники
+    [],[],       // 7-8  - Трёхпалубники
+    [],          // 9    - Однопалубники
+];
+
+
+
+
 //Однопалубники
 let on1 = 1
 let on2 = 1
 let on3 = 1
 let on4 = 1
-//Двухпалубники
+//Трёхпалубники
 let db1 = 2
 let db2 = 2
 let db3 = 2
-//Трёхпалубники
+//Двухпалубники
 let Tr1 = 3
 let Tr2 = 3
 //Однопалубники
@@ -37,13 +67,7 @@ var hit= 0;
 
 let dead_ship = [];
 
-//Массив для регистрации попадания по кораблю.
-let hit_ship = [
-    [],[],[],[], //0-3   - Однопалубники
-    [],[],[],    //4-6   - Двухпалубники
-    [],[],       // 7-8  - Трёхпалубники
-    [],          // 9    - Однопалубники
-];
+
 
 
 
@@ -54,6 +78,14 @@ let hit_ship = [
     [id_N, id_N, id_N, id_N] // чётырёхпалубник и его КП
     и тд.
 ]; 
+*/
+
+/* //Цикл, который выполнит действие, а после проверит.
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while (i < 3);
 */
 
 
@@ -120,52 +152,91 @@ function handleClick(event){
     let att= document.getElementById(event.srcElement.id)
 
     /*--------------------------------------------*/
-
-    // Регестрация попадания.
-    if(field_xy[x_coord][y_coord]=="Tr1"){
-        hit_ship[7].push(att)
-        att.style.backgroundColor = '#ffa1a1'
-        Tr1 = Tr1 - 1
-        hit = hit + 1
-    
-    } else if(field_xy[x_coord][y_coord]=="Ch"){
+    /*if(field_CanShot[x_coord][y_coord]==true){
+        field_CanShot[x_coord][y_coord] = false//Проверка на выстрел 
         
-        hit_ship[9].push(att)
-        att.style.backgroundColor = '#ffa1a1'
-        Ch = Ch - 1
-        hit = hit + 1
-    
-    }
-    else {
-        att.style.backgroundColor = '#b882ff'
-    }
+        //Проверка на попадания по карабл, и если он попал, то данные изменяются и переносятся в массив data_ship
+        
+        for(g = 0;g<10;g++){
+            if(field_xy[x_coord][y_coord]==data_ship[g][0]){//Если попал
+                hp = data_ship[g][1]
+                console.log(att)
+                hp = hp-1
+                hit = hit + 1
+                data_ship[g][1] = hp
+                hit_ship[g].push(att)
+                console.log(hit_ship[g])
+                att.style.backgroundColor = '#ffa1a1' //Цвет, что ты попал.
 
-    /*--------------------------------------------*/
+            }if (field_xy[x_coord][y_coord]==0){ //Промах.
+                att.style.backgroundColor = '#b882ff' //Цвет, что ты промазал.
+            }
+        }
+        for (g1=0;g1<10;g1++){
+            gh=g1
+            if(data_ship[gh][1]==0){
+                console.log(data_ship[gh][0])
+            }
+        }
+        if(hit == 7){
+            setInterval(function(){
+                let result2 = document.getElementById("Win_Lozz")
+                result2.innerText = "Вы уничтожели все корабли.";
+            }, 200);
+        }
+    }*/
     
-    //Проверка при потопление корабля.
-    if (Tr1==0){
-        for(var arr = 0; arr < 3; arr++){
-            stl = hit_ship[7][arr];
-            stl.style.backgroundColor = '#3dffae'
+    if(field_CanShot[x_coord][y_coord]==true){//Проверка на выстрел 
+        field_CanShot[x_coord][y_coord] = false
+        
+        // Регестрация попадания.
+
+        if(field_xy[x_coord][y_coord]=="Tr1"){
+            hit_ship[7].push(att)
+            att.style.backgroundColor = '#ffa1a1'
+            Tr1 = Tr1 - 1
+            hit = hit + 1
+        
+        } else if(field_xy[x_coord][y_coord]=="Ch"){
+            
+            hit_ship[9].push(att)
+            att.style.backgroundColor = '#ffa1a1'
+            Ch = Ch - 1
+            hit = hit + 1
+        
+        }
+        else {
+            att.style.backgroundColor = '#b882ff'
+        }
+
+        //--------------------------------------------
+        
+        //Проверка при потопление корабля.
+        if (Tr1==0){
+            for(var arr = 0; arr < 3; arr++){
+                stl = hit_ship[7][arr];
+                stl.style.backgroundColor = '#3dffae'
+            }
+        }
+        if (Ch==0){
+            for(var arr = 0; arr < 4; arr++){
+                stl = hit_ship[9][arr];
+                stl.style.backgroundColor = '#3dffae'
+            }
+        }
+
+        //--------------------------------------------
+
+        //Результат при потапление кораблей
+        if (hit==7){
+            //Таймер, перед объевлением. 
+            setInterval(function(){
+                let result2 = document.getElementById("Win_Lozz")
+                result2.innerText = "Вы уничтожели все корабли.";
+            }, 200);
         }
     }
-    if (Ch==0){
-        for(var arr = 0; arr < 4; arr++){
-            stl = hit_ship[9][arr];
-            stl.style.backgroundColor = '#3dffae'
-        }
-    }
-
-    /*--------------------------------------------*/
-
-    //Результат при потапление кораблей
-    if (hit==7){
-        //Таймер, перед объевлением. 
-        setInterval(function(){
-            let result2 = document.getElementById("Win_Lozz")
-            result2.innerText = "Вы уничтожели все корабли.";
-        }, 200);
-    }
+        
 }
 
 
