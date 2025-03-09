d7c688ede89932d85708b9209eab1a81d84e8334
Katal Vlad <kavladpost@gmail.com>
2025-03-09 17:16:09 +0800

    Частично рабочие кораблики
    Главное:
    
    >>Добавлено
    1. Второе поле.
    2. Добавлено два массива под второе поле.
    
    >> Реализовано
    1. Расстановка кораблей на втором поле.
    2. Система ключей (Пока игрок не поставит свои корабли, игра не запустится.
    3. При старте игры, кнопки "Рандом" и "Играть" исчезают.
    
    >> Исправлено
    
    1.  Исправлен баг с появлением надпись "Победа", когда тот появлялся раньше времени.
    
    По мелочи:
    1. Избавился от лишних комментарий.
    2. Избавился от  не нужных переменных и массива, которые никак не участвуют в коде.
    3. Более лучше оформлен код.

--- Пробный/js/script.js
+++ Пробный/js/script.js
@@ -1,6 +1,6 @@
+//Массивы для отображения поля. (в системе)
 
-//Массив, куда помещаются корабли.
-let field_xy = [
+let field_xy = [//Поле игрока
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -13,7 +13,22 @@ let field_xy = [
     [0,0,0,0,0,0,0,0,0,0]
 ];
 
-let field_CanShot= [
+let field_xy2 = [//Поле робота.
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+];
+
+//Массивы для разришения что-то на противоположном поле.(в системе)
+
+let field_CanShot= [//Поле игрока для проверки выстрела.
     [true,true,true,true,true,true,true,true,true,true,],
     [true,true,true,true,true,true,true,true,true,true,],
     [true,true,true,true,true,true,true,true,true,true,],
@@ -25,95 +40,63 @@ let field_CanShot= [
     [true,true,true,true,true,true,true,true,true,true,],
     [true,true,true,true,true,true,true,true,true,true,]
 ]
-//Корабли и их ХП (кол-во целых палуб.)
+
+let field_CanShot2= [//Поле робота для проверки выстрела.
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,],
+    [true,true,true,true,true,true,true,true,true,true,]
+]
+
+/*-----------------------------------------*/
 
 //Массив, в котором хронятся данные: (0)Имя, (1)кол-во палуб или оставшихся палуб, а в дальнейшем Id div-ов, чтобы осуществить пометку убитого коробля. 
+
 data_ship = [
     ["Ch",4],//Однопалубники
     ["Tr1",3],["Tr2",3],//Трёхпалубники
     ["db1",2],["db2",2],["db3",2], //Двухпалубники
     ["on1",1],["on2",1],["on3",1],["on4",1] //Однопалубники
 ]
+
+data_ship2 = [
+    ["Ch",4],//Однопалубники
+    ["Tr1",3],["Tr2",3],//Трёхпалубники
+    ["db1",2],["db2",2],["db3",2], //Двухпалубники
+    ["on1",1],["on2",1],["on3",1],["on4",1] //Однопалубники
+]
+
+/*-----------------------------------------*/
+
+//Переменные
+
 Ship_Sum = 10
-//Массив для регистрации попадания по кораблю.
-let Position_ship = [
-    [],[],[],[], //0-3   - Однопалубники
-    [],[],[],    //4-6   - Двухпалубники
-    [],[],       // 7-8  - Трёхпалубники
-    [],          // 9    - Однопалубники
-];
+
 
 var dead_ship = 0
+var dead_ship2 = 0
 
 var triger = 0
-
-/*
-//Однопалубники
-let on1 = 1
-let on2 = 1
-let on3 = 1
-let on4 = 1
-//Трёхпалубники
-let db1 = 2
-let db2 = 2
-let db3 = 2
-//Двухпалубники
-let Tr1 = 3
-let Tr2 = 3
-//Однопалубники
-let Ch = 4;
-*/
-/*-----------------------*/ 
-
-var b = 0;
-var hit= 0;
+var key_play = false
 
 
+/*-----------------------------------------*/
 
 
 
 
 //Моё виденье попаданий кораблей
-/*let hit_ship = [
-    // Координаты Попадания (КП) могут быть в разном порядек, не важно как начать бить корабль
-    [id_N, id_N, id_N] // трёхпалублин и его КП
-    [id_N, id_N, id_N, id_N] // чётырёхпалубник и его КП
-    и тд.
-]; 
-*/
-
-/* //Цикл, который выполнит действие, а после проверит.
-let i = 0;
-do {
-  alert( i );
-  i++;
-} while (i < 3);
-*/
-
 
 // Можно - true, нельзя - false не знаю англиский
+
 //var field = document.querySelector('.Field'); Класс
 
-/*var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "white";
-div.innerHTML = "Hello";
-console.log(div);
-document.getElementById("field").appendChild(div);*/
-/*for (i=0;i<10;i++) {
-    var div = document.createElement("div");
-    div.style.width = "33px";
-    div.style.height = "33px";
-    div.style.background = "red";
-    div.style.color = "white";
-    div.innerHTML = "Hello";
-    console.log(div);
-    document.getElementById("field").appendChild(div);
-};*/
-
-//el.setAttribute('id', 'my-id');
 
 /*for (i=0;i<2;i++) {
     for (a=0;a<10;a++){
@@ -125,27 +108,61 @@ document.getElementById("field").appendChild(div);*/
     
 };*/
 
-var wrapperBlock = document.getElementById("wrapperBlock");
-let Rand = document.getElementById("Random")
-Rand.addEventListener('click', Random)//Вызвать функцию рандом при клике.
-window.onload = function(){
 
-   for(i=0;i<10;i++){
+/*-----------------------------------------*/ 
+
+// Берём места, куда поместим 2 поля (визуально).
+
+var wrapperBlock = document.getElementById("wrapperBlock");
+var wrapperBlock2 = document.getElementById("wrapperBlock2");
+
+/*-----------------------------------------*/ 
+
+//Кнопки.
+
+
+//Вызвать функцию рандом, чтобы рандомно раставить корабли.
+let Rand = document.getElementById("Random")
+Rand.addEventListener('click', Random)
+
+
+//Запустить игру.
+let Play = document.getElementById("Play")
+Play.addEventListener('click', Play_game)
+
+
+/*-----------------------------------------*/ 
+
+//Создаём 2 поля.
+
+for(i=0;i<10;i++){
     
-        // создаем строку  
-        for(j=0;j<10;j++){
-            var square = document.createElement("div");
-            square.classList.add("square");
-            square.id=i+";"+j;
-            square.addEventListener('click',handleClick);
-            square.innerText = " ";
-            wrapperBlock.appendChild(square)
-        }
-  }
+    // Поле 1 
+    for(j=0;j<10;j++){
+        var square = document.createElement("div");
+        square.classList.add("square");
+        square.id=i+";"+j;
+        square.innerText = " ";
+        wrapperBlock.appendChild(square)
+    }
+    // Поле 2
+    for(j=0;j<10;j++){
+        var square2 = document.createElement("div");
+        square2.classList.add("square2");
+        square2.id=i+";;"+j;
+        square2.addEventListener('click',handleClick);
+        square2.innerText = " ";
+        wrapperBlock2.appendChild(square2)
+    }
 }
+
+/*-----------------------------------------*/ 
+
+//Функции пользователя.
 
 function Random(){//Создание кораблей по методу рандома.
     //Проверяем, был ли поле уже запонино. 0 - не было установленые корабли, 1 - уже установленны корабли, нужно их удалить их.
+    key_play = true
     if(triger == 1){
         for(xd=0;xd<10;xd++){
             for(yd=0;yd<10;yd++){
@@ -218,7 +235,6 @@ function Random(){//Создание кораблей по методу ранд
                     document.getElementById(x5+";"+y5).classList.add("Ship_shadow")
                     field_xy[x5][y5] = Name_Ship
                 }
-                console.log(field_xy)
                 for(c=0;c<vr_coord.length;c++){ //Обводка вокруг корабля.
                     let vr_Check_Ship =[]
                     //Начальные координаты палубы корабля.
@@ -267,6 +283,19 @@ function Random(){//Создание кораблей по методу ранд
 
 }
 
+function Play_game(){
+    if(key_play == true){
+        if(Random2()==true){
+            Rand.style.display = "none"
+            Play.style.display = "none"
+        }
+    }else{
+        alert("Пожалуйта, раставте корабли.")
+    }
+    
+
+}
+
 function Delet(xd,yd){
     if (field_xy[xd][yd]==0){ // Нет в массиве ничего
         return 0
@@ -296,11 +325,9 @@ function checkShipBoard(vr_coord){ //Проверяем можно ли пост
 
 
 }
-
-
 function handleClick(event){//Выстрел.
     // Берём id div-ва и разделяем на координаты Х и У.
-    coords=event.srcElement.id.split(";");
+    coords=event.srcElement.id.split(";;");
     x_coord=coords[0];
     y_coord=coords[1]; 
 
@@ -310,30 +337,32 @@ function handleClick(event){//Выстрел.
 
 
     /*--------------------------------------------*/
-    if(field_CanShot[x_coord][y_coord]==true){//Проверка на выстрел 
-        field_CanShot[x_coord][y_coord] = false
+    if(field_CanShot2[x_coord][y_coord]==true){//Проверка на выстрел 
+        field_CanShot2[x_coord][y_coord] = false
         
         //Проверка на попадания по карабл, и если он попал, то данные изменяются и переносятся в массив data_ship
         
         for(g = 0;g<10;g++){
-            if(field_xy[x_coord][y_coord]==data_ship[g][0]){//Если попал
-                hp = data_ship[g][1]
+            if(field_xy2[x_coord][y_coord]==data_ship[g][0]){//Если попал
+                hp = data_ship2[g][1]
                 hp = hp-1
-                hit = hit + 1
-                data_ship[g][1] = hp
-                data_ship[g].push(att)
+                data_ship2[g][1] = hp
+                data_ship2[g].push(att)
                 att.style.backgroundColor = '#ffa1a1' //Цвет, что ты попал.
 
-            }else if(field_xy[x_coord][y_coord]==0 || field_xy[x_coord][y_coord]==1){ //Промах.
+            }else if(field_xy2[x_coord][y_coord]==0 || field_xy2[x_coord][y_coord]==1){ //Промах.
                 att.style.backgroundColor = '#b882ff' //Цвет, что ты промазал.
             }
             //Мертвяк.
-            if (data_ship[g][1]==0){
-                for(k=2;k<data_ship[g].length;k++){
-                    stl= data_ship[g][k]
+            if (data_ship2[g][1]==0){
+                for(k=2;k<data_ship2[g].length;k++){
+                    stl= data_ship2[g][k]
                     stl.style.backgroundColor = '#3dffae'
                 }
                 dead_ship = dead_ship + 1
+                data_ship2[g][1] = "dead"
+                console.log(dead_ship)
+                console.log(data_ship2[g])
             }    
         }
         
@@ -346,5 +375,142 @@ function handleClick(event){//Выстрел.
     }
       
 }
+
+/*-----------------------------------------*/ 
+
+//Функции робота.
+
+function Random2(){//Создание кораблей по методу рандома.
+    //Проверяем, был ли поле уже запонино. 0 - не было установленые корабли, 1 - уже установленны корабли, нужно их удалить их.
+    //Определяем расположение по вертикали или по горизонтали.ы
+    // 0 - горизонталь; 1 - вертекаль
+    //remove('active')
+    //Добавить отчистку, если игрок решит заново пересгенерировать положение кораблей.
+    for(r=0;r<Ship_Sum;r++){
+        Name_Ship = data_ship2[r][0]//Название корабля.
+        dask = data_ship2[r][1]//Кол-во палуб.
+        let vr_coord =[]//Временый массив, мнимых координат корабля на поле.
+        Wh_tf=true
+        /* 
+            Цикл while нужен, чтобы генерировать координаты и проверять(функция checkShipBoard), можно ли на этих координатах ставить корабль.
+            Есла "да", то выполнится функция и прервётся цикл while.
+            Если "нет", то цикл не завершится и заново будет генерировать координаты.
+        */
+        while(Wh_tf == true){
+            var xy = Math.round(Math.random()) //Рандомное число от 0 до 1
+            if (xy==0){ // 0 - горизонталь
+                min = 0
+                max = 9
+                x = Math.floor(Math.random() * (max - min + 1)) + min
+                y = Math.floor(Math.random() * (max - dask - min + 2)) + min;//Диапозон с учётом с размером корабля.
+                absl = max - dask+1 //Максимум по координатам, котором можно поставить корабль,
+                                    //чтобы не зайти за поле.
+                alf = dask//Временный массив координат кораблей.
+                
+                /*--------------------------------------------------------------------------------- */
+        
+                //Из первой координаты создаём уже сам корабль.
+                for (i=0;i<alf;i++){
+                    
+                    vr_coord[i] = [x,y]//Координаты временного массива.
+                    y= y+1
+                }
+        
+                
+            }else{     // 1 - вертекаль
+                min = 0 
+                max = 9
+                x = Math.floor(Math.random() * (max - dask - min + 2)) + min;//Диапозон с учётом с размером корабля.
+                y = Math.floor(Math.random() * (max - min + 1)) + min
+                absl = max - dask+1
+                alf = dask//Координаты временного массива.
+        
+                /*--------------------------------------------------------------------------------- */
+        
+                //Из первой координаты создаём уже сам корабль.
+                for (i=0;i<alf;i++){
+                    vr_coord[i] = [x,y]
+                    x= x+1
+                }    
+            }
+            if(checkShipBoard2(vr_coord)==true){
+                
+                for(r1=0;r1<vr_coord.length;r1++){
+                    x5=vr_coord[r1][0]
+                    y5=vr_coord[r1][1]
+                    document.getElementById(x5+";;"+y5).classList.add("Ship_shadow")
+                    field_xy2[x5][y5] = Name_Ship
+                }
+                for(c=0;c<vr_coord.length;c++){ //Обводка вокруг корабля.
+                    let vr_Check_Ship =[]
+                    //Начальные координаты палубы корабля.
+                    x1=vr_coord[c][0]
+                    y1=vr_coord[c][1]
+                    y1=y1+1 //Сдвиг вправо 1 раз.
+                    vr_Check_Ship.push([x1,y1])
+                    x1=x1+1//Сдвиг вниз 1 раз.
+                    vr_Check_Ship.push([x1,y1])
+                    for(t=0;t<2;t++){//Сдвиг влево 2 раза.
+                        y1=y1-1
+                        vr_Check_Ship.push([x1,y1])
+                    }
+                    for(tg=0;tg<2;tg++){//Сдвиг вверх 2 раза
+                        x1=x1-1
+                        vr_Check_Ship.push([x1,y1])
+                    }
+                    for(th=0;th<2;th++){//Сдвиг право 2 раза
+                        y1=y1+1
+                        vr_Check_Ship.push([x1,y1])
+                    }
+                    //console.log(vr_Check_Ship) //Координаты вокруг клетки.
+            
+                    //Проверка координат, чтобы пометить поле возле корабля.
+                    for(chk=0;chk<vr_Check_Ship.length;chk++){
+                        x2= vr_Check_Ship[chk][0]
+                        y2= vr_Check_Ship[chk][1]
+                        if (x2>-1 && x2<10){ //9>=x2>=0
+                            if (y2>-1 && y2<10){
+                                if(field_xy2[x2][y2]==0){
+                                    field_xy2[x2][y2] = 1
+                                }
+                            }
+                            
+                        }
+                    }
+            
+                }
+                Wh_tf = false
+            }
+
+        }//Конец цикла while
+        
+    }//Конец цикла for
+    
+    return true
+}
+
+function checkShipBoard2(vr_coord){ //Проверяем можно ли поставить корабль
+    
+    Can=0
+    for(i=0;i<vr_coord.length;i++){
+        x4= vr_coord[i][0]
+        y4= vr_coord[i][1]
+        if(field_xy2[x4][y4] == 0){
+            Can=Can+1    
+        } else {
+            break;
+        }
+    }
+    if(Can==vr_coord.length){
+        return true;
+    }else{
+        return false;
+    } 
+    
+
+
+}
+
+
 
 
