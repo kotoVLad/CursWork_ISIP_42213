72a4b3c3babfd41b1657aca770f7267c00cecdf1
Katal Vlad <kavladpost@gmail.com>
2025-04-09 00:02:25 +0800

    Пока только теоретически сделал соединение.

--- test_server/server.js
+++ test_server/server.js
@@ -32,35 +32,57 @@ let key_click = 0
 let rooms//Комната
 
 // Обработка подключений Socket.IO
-//[[Название комнаты, id-user1, id-user2]]
-let room_using = []//Уже созданные комнаты.
+//[[Название комнаты, user1={Ник,id-user1},user2={Ник,id-user2}] //Комната[n][0], status[n][1]...
+let room_using = []//Уже созданные комнаты.  Комната 1 = 0 в массиве и тд.
 let room_using_person=[]//Для персональных.
 
 io.on('connection', (socket) => {
-    user = socket.id
-    console.log('Коннект пользователя', user);
+    console.log('Коннект пользователя');
 
-    key_click++
-    client++
+    socket.on('cre_con_room', (data)=>{
+        console.log("-----------------------------")
+        user = socket.id
+        console.log('Id-пользователя:',user);
+        Nick=data.Nick
+        ID_user=data.ID_user
+        key_click++
+        client++
+        rooms=Math.round(client/2)
 
+        if(key_click==1){
 
-    rooms=Math.round(client/2)
-    console.log(rooms)
-    socket.join(Math.round(client/2))
-    socket.emit('serverMsg', Math.round(client/2))
-    
-    
+            socket.join(rooms)
 
-    if(key_click==1){
-        console.log("Ждём второго игрока.")
-        room_using.push([rooms])
-        console.log(JSON.stringify(room_using))
-        
-    }else{
-        key_click=0
-        io.to(rooms).emit('CanClick')
-        //socket.emit('CanClick', true)
-    }
+            console.log("Создана комната:",rooms)
+            console.log("Ждём второго игрока.")
+
+            room_using.push([rooms,{Nick:Nick,ID_user:ID_user}])
+            
+            console.log(JSON.stringify(room_using))
+            console.log("-----------------------------")
+
+            socket.emit('serverMsg', Math.round(client/2))
+            
+            
+        }else{
+            socket.join(rooms)
+            key_click=0
+            a = rooms-1
+            console.log("Второй игрок зашёл в комнту:",rooms)
+            for(i=0;i<room_using.length;i++){
+                if(room_using[i][0]==rooms){
+                    room_using[i].push({Nick:Nick,ID_user:ID_user})
+                    break
+                }
+            }
+            console.log(JSON.stringify(room_using))
+            console.log("-----------------------------")
+
+            socket.emit('serverMsg', rooms)
+            io.to(rooms).emit('CanClick')
+            //socket.emit('CanClick', true)
+        }
+    })
 
     // Можно добавить дополнительные обработчики событий здесь
     socket.on('ButtonClick',clientRoom=>{
--- test_server/static/javascript/test.js
+++ test_server/static/javascript/test.js
@@ -5,17 +5,20 @@ let activityInterval;
 
 var Button = document.getElementById("clicking")
 var Button2 = document.getElementById("Ck")
+var Button3 = document.getElementById("Conect_room")
+
 var label = document.getElementById("label")
 
 const User1_status = document.getElementById('status');//Мой статус
 const User2_status = document.getElementById('status2');//Статус второго человека
 
 const mark_status1 = document.getElementById('stat');
-const mark_status2 = document.getElementById('stat');
+const mark_status2 = document.getElementById('stat2');
 
 
-Button.addEventListener("click", conn)
-Button2.addEventListener("click", conne)
+Button.addEventListener("click", conn)//Кнопка кликать.
+Button2.addEventListener("click", conne)//Кнопка для подключение к серверу.
+Button3.addEventListener("click", cre_con_room)//Создать/Подклються к комнате.
 
 var Nick = document.getElementById("Name")//Ник пользователя.
 var ID_user = document.getElementById("id_user")//Id пользователя.
@@ -26,12 +29,9 @@ function conne(){
         socket = io();
         Button2.textContent = 'Отключиться';
 
-        User1_status.textContent = 'Ваш статус: подключен' 
+        mark_status1.textContent = 'подключен' 
         mark_status1.classList.remove("red")
         mark_status1.classList.remove("add")
-
-        Nick = Nick.value;
-        ID_user = ID_user.value;
 
         //--------------------------------------
         socket.on('serverMsg',(data)=>{
@@ -41,6 +41,11 @@ function conne(){
         socket.on('CanClick',()=>{
             key_click = true
             label.innerText="Можно кликать"
+            mark_status2.textContent = 'подключен' 
+            
+            mark_status2.classList.remove("red")
+            mark_status2.classList.remove("add")
+            
             label.classList.remove("red")
             label.classList.add("can")
         })
@@ -62,9 +67,15 @@ function conne(){
     
 }
 
-
 function conn(){
     if(key_click==true){
         socket.emit('ButtonClick',clientRoom)
     }
+}
+
+function cre_con_room(){
+    Button3.textContent = "Отключиться от комнаты"
+    Nick = Nick.value;
+    ID_user = ID_user.value;
+    socket.emit('cre_con_room',{Nick:Nick, ID_user:ID_user})
 }
