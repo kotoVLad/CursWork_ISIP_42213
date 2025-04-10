f617c935679577173a873ad73f002b58d0be0009
Katal Vlad <kavladpost@gmail.com>
2025-04-10 21:11:10 +0800

    +-

--- test_server/server.js
+++ test_server/server.js
@@ -6,8 +6,8 @@ const path = require('path');
 
 
 var UserRouter = require('./routes/user');
-
-
+const { emit } = require('process');
+const { Console } = require('console');
 
 // Настройка EJS
 app.set('views', path.join(__dirname, 'views'));
@@ -35,20 +35,27 @@ let rooms//Комната
 //[[Название комнаты, user1={Ник,id-user1},user2={Ник,id-user2}] //Комната[n][0], status[n][1]...
 let room_using = []//Уже созданные комнаты.  Комната 1 = 0 в массиве и тд.
 let room_using_person=[]//Для персональных.
-
+let userSockets={}
 io.on('connection', (socket) => {
-    console.log('Коннект пользователя');
+    //Тут должны создаваться кастомные ID
+    socket.on('Id_identified',(id)=>{
+        console.log("-----------------------------")
+        console.log('Коннект пользователя');
+        user = socket.id
+        console.log('Id-пользователя:',user);
+        ID_user=id
+        userSockets[ID_user] = socket
+        console.log('Кастомный-Id:',ID_user);
+        console.log("-----------------------------")
+    })
 
     socket.on('cre_con_room', (data)=>{
         console.log("-----------------------------")
-        user = socket.id
-        console.log('Id-пользователя:',user);
         Nick=data.Nick
         ID_user=data.ID_user
         key_click++
         client++
         rooms=Math.round(client/2)
-
         if(key_click==1){
 
             socket.join(rooms)
@@ -60,18 +67,28 @@ io.on('connection', (socket) => {
             
             console.log(JSON.stringify(room_using))
             console.log("-----------------------------")
-
-            socket.emit('serverMsg', Math.round(client/2))
             
             
         }else{
             socket.join(rooms)
             key_click=0
-            a = rooms-1
             console.log("Второй игрок зашёл в комнту:",rooms)
             for(i=0;i<room_using.length;i++){
                 if(room_using[i][0]==rooms){
                     room_using[i].push({Nick:Nick,ID_user:ID_user})
+
+                    console.log("Отправка User")
+                    use1 = room_using[i][1].ID_user//id 1 пользователя комнаты
+                    socket = userSockets[use1];//Soket 1 пользователя комнаты
+                    Room_us2 = room_using[i][2].Nick//Ник 2 пользователя комнаты
+                    console.log(Room_us2)
+                    socket.emit('Cann_us', Room_us2)
+                    
+                    use2 = room_using[i][2].ID_user//id 2 пользователя комнаты
+                    socket = userSockets[use2];//Soket 2 пользователя комнаты
+                    Room_us1 = room_using[i][1].Nick//Ник 1 пользователя комнаты
+                    socket.emit('Cann_us', Room_us1)
+                    
                     break
                 }
             }
@@ -82,15 +99,50 @@ io.on('connection', (socket) => {
             io.to(rooms).emit('CanClick')
             //socket.emit('CanClick', true)
         }
+        socket.emit('serverMsg', Math.round(client/2))
+
+        console.log("-----------------------------")
+        Users = Object.keys(userSockets)
+        console.log("Текущие пользователи:", Users);
+        console.log("-----------------------------")
     })
 
     // Можно добавить дополнительные обработчики событий здесь
     socket.on('ButtonClick',clientRoom=>{
         io.to(clientRoom).emit('EventServer')//Отобразить у всех действие.
     })
-    socket.on('disconnect', () => {
+    socket.on('disconnect', (Room_dis) => {
         console.log('Пользователь отключился');
     });
+
+    socket.on('leave_room', (data)=>{
+        if(socket.rooms.has(data.clientRoom)){
+            if(!data.user_triger){
+                key_click=0
+                client--
+                console.log(`1.Пользователь ${data.Nick} вышел из комнаты ${data.clientRoom}, комната была удалена.`)
+                socket.leave(data.clientRoom);
+
+            }if(data.user_triger==true){
+                console.log(`2.Пользователь ${data.Nick} вышел из комнаты ${data.clientRoom}.`)
+                socket.leave(data.clientRoom);
+                io.to(data.clientRoom).emit('User2_discon',data.Nick)
+
+            }if(data.user_triger=="end"){
+                console.log(`3.Пользователь ${data.Nick} вышел из комнаты ${data.clientRoom}, комната была удалена.`)
+                socket.leave(data.clientRoom);
+                for(i=0;i<room_using.length;i++){
+                    if(room_using[i][0]==data.clientRoom){
+                        console.log(`Комната ${data.clientRoom} была удалена из массива`)
+                        room_using.splice(i,1)
+                        console.log(JSON.stringify(room_using))
+                        break
+                    }
+                }
+            }
+            
+        }
+    })
 });
 
 // Запуск сервера
--- test_server/static/javascript/test.js
+++ test_server/static/javascript/test.js
@@ -6,11 +6,16 @@ let activityInterval;
 var Button = document.getElementById("clicking")
 var Button2 = document.getElementById("Ck")
 var Button3 = document.getElementById("Conect_room")
+var Button4 = document.getElementById("leave_room")
+
+var Button_save = document.getElementById("Save")
 
 var label = document.getElementById("label")
 
 const User1_status = document.getElementById('status');//Мой статус
 const User2_status = document.getElementById('status2');//Статус второго человека
+
+const User2 = document.getElementById('user2')
 
 const mark_status1 = document.getElementById('stat');
 const mark_status2 = document.getElementById('stat2');
@@ -19,16 +24,20 @@ const mark_status2 = document.getElementById('stat2');
 Button.addEventListener("click", conn)//Кнопка кликать.
 Button2.addEventListener("click", conne)//Кнопка для подключение к серверу.
 Button3.addEventListener("click", cre_con_room)//Создать/Подклються к комнате.
+Button4.addEventListener("click", leave)
+Button_save.addEventListener("click",Saveing)
 
 var Nick = document.getElementById("Name")//Ник пользователя.
 var ID_user = document.getElementById("id_user")//Id пользователя.
-
-let clientRoom;//Название комнаты
+var Nick_user=null
+var ID_User=null
+var user_triger = null//null - 2 пользователь не заходил, true - Зашёл, end - пользователь покинул.
+let clientRoom=0;//Название комнаты
 function conne(){
     if(!socket){
         socket = io();
         Button2.textContent = 'Отключиться';
-
+        socket.emit('Id_identified',ID_User)
         mark_status1.textContent = 'подключен' 
         mark_status1.classList.remove("red")
         mark_status1.classList.remove("add")
@@ -38,13 +47,19 @@ function conne(){
             console.log(`Я в комнате${data}`)
             clientRoom = data;
         })
+
+        socket.on('Cann_us',(data)=>{
+            User2.textContent = data
+        })
+
         socket.on('CanClick',()=>{
             key_click = true
+            user_triger = true
             label.innerText="Можно кликать"
             mark_status2.textContent = 'подключен' 
             
             mark_status2.classList.remove("red")
-            mark_status2.classList.remove("add")
+            mark_status2.classList.add("can")
             
             label.classList.remove("red")
             label.classList.add("can")
@@ -57,9 +72,16 @@ function conne(){
                 Button.classList.add("colr")
             }
         })
+
+        socket.on('User2_discon', (User2_dis)=>{
+            mark_status2.textContent="Отключился."
+            mark_status2.classList.remove("can")
+            mark_status2.classList.add("red")
+            user_triger="end"
+        })
     }else {
         // Закрываем существующее подключение
-        socket.disconnect();
+        socket.disconnect(clientRoom);
         socket = null;
         // Обновляем UI
         Button2.textContent = 'Подключиться';
@@ -74,8 +96,28 @@ function conn(){
 }
 
 function cre_con_room(){
-    Button3.textContent = "Отключиться от комнаты"
-    Nick = Nick.value;
-    ID_user = ID_user.value;
-    socket.emit('cre_con_room',{Nick:Nick, ID_user:ID_user})
+    socket.emit('cre_con_room',{Nick:Nick_user, ID_user:ID_User})
+}
+
+function leave(){
+    if(clientRoom!=0){
+        User2.textContent = "Чужой"
+        mark_status2.textContent="Отключился."
+        mark_status2.classList.remove("can")
+        mark_status2.classList.add("red")
+
+        socket.emit('leave_room',{clientRoom:clientRoom,user_triger:user_triger,Nick:Nick_user})
+
+        clientRoom=0
+        if(user_triger=="end"||user_triger==true){
+            user_triger=null
+        }
+    }
+    
+}
+
+function Saveing(){
+    Nick_user = Nick.value;
+    ID_User = ID_user.value;
+    console.log(Nick_user,ID_User)
 }
